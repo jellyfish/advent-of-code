6ba7ce6787195e5afb62b503353d648b53a8959f
Spencer Plant <splant.10@gmail.com>
2022-12-07 20:26:01 -0700

    Improvements to day generator

--- utils/day_generator.rb
+++ utils/day_generator.rb
@@ -16,18 +16,36 @@ class DayGenerator
     @template ||= <<~RUBY
       # frozen_string_literal: true
 
+      require_relative '../../utils/file_manager'
+      require_relative '../../utils/printer'
+
       module Year#{@year}
         class Day#{@day}
           def initialize
           end
 
           def part1
+            # input_file.readlines.each do |line|
+            # end
+            "TODO"
           end
 
           def part2
+            "TODO"
+          end
+
+          private
+
+          def input_file
+            FileManager.input_file(#{@year}, #{@day})
           end
         end
       end
+
+      day = Year#{@year}::Day#{@day}.new
+      Printer.frame('Part 1') { day.part1 }
+      Printer.candycane
+      Printer.frame('Part 2') { day.part2 }
     RUBY
   end
 end
